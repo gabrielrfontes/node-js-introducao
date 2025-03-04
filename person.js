27d47f0b900f7332a69172d319cd7872e110ded3
Gabriel <garfontes@gmail.com>
2025-03-04 13:21:23 -0300

    Introduçao ao Node.JS

---
+++ person.js
@@ -0,0 +1,15 @@
+class Person {
+    constructor(name) {
+        this.name = name
+    }
+
+    sayMyName() {
+        return `Hello! My name is ${this.name}!`
+    }
+    
+}
+
+
+module.exports = {
+    Person,
+}
